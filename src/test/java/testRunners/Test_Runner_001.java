701c5ad6a302e8daf3e4f7f72b3b7c1f37304728
bhanusurya08134 <bhanusurya08134@gmail.com>
2024-12-04 14:17:09 +0530

    Update Test_Runner_001.java

--- src/test/java/testRunners/Test_Runner_001.java
+++ src/test/java/testRunners/Test_Runner_001.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Parameters;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features",tags= "@tag1",glue = "stepDefinitions", monochrome = true,plugin = { "html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+@CucumberOptions(features = "src/test/java/features",tags= "@tag1",glue = "stepDefinitions", monochrome = true,plugin = { "json:target/cucumber.json", "html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class Test_Runner_001 extends AbstractTestNGCucumberTests{
 	
 
